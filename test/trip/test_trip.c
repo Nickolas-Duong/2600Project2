1c057716b5179d4795e05bc9b918232a197f724d
Dylan Oliva <djoliva@cpp.edu>
2022-11-29 10:03:50 -0800

    added testing file for trip

--- test/trip/test_trip.c
+++ test/trip/test_trip.c
@@ -5,8 +5,15 @@
 
 #include "../../include/trip.h"
 #include "../../include/display.h"
+#include "../../include/input.h"
 
 int main()
 {
-  
+    printf("What is your current total expenses:");
+    double expenses;
+    scanf("%lf", &expenses);
+
+    expenses = trip(expenses);
+
+    display_total(5, 11, 11, expenses, 500, 0, 0);
 }
