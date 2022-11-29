6caea26acfa676a51baff4804b0a63f6122a5de6
Dylan Oliva <djoliva@cpp.edu>
2022-11-29 10:53:46 -0800

    fixed method declaration mistake

--- include/trip.h
+++ include/trip.h
@@ -9,7 +9,7 @@
 double trip(double total_expenses); //calls each other method using a menu
 double get_Car_Rentals(); //prompts user to input car rental cost
 double get_Private_Vehicle_Used(); //prompts user to input gas cost
-double get_Airfare_Cost(); //ask user cost of one ticket if a plane was used
+double get_Round_Trip_Airfare(); //ask user cost of one ticket if a plane was used
 double get_Parking_Fees(); //ask user to input any parking fees
 double get_Taxi_Fees(); //ask user to input any taxis cost
 
