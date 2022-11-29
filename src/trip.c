9b55e1bbf71f906e1f91efea9dcd3767b3fdb321
Dylan Oliva <djoliva@cpp.edu>
2022-11-29 09:38:05 -0800

    removed redundant menues and fixed the exit input

--- src/trip.c
+++ src/trip.c
@@ -10,7 +10,7 @@ double trip(double total_expenses){
         printf("3 - Taxi Cost");
         printf("4 - Parking Fees");
         printf("5 - Airfare Cost");
-        printf("Any key to exit");
+        printf("0 - Exit");
         option = getChar();
         getChar();
 
@@ -37,111 +37,46 @@ double trip(double total_expenses){
 }
 
 double get_Car_Rentals(){
-    char option;
     double carRentals;
 
-    do{
-        printf("Select an option:");
-        printf("1 - Input a Car Rental Fee");
-        printf("2 - Exit");
-        option = getChar();
-        getChar();
-
-        if(option == '1'){
-            double temp = 0;
-            printf("How much was the car rental:");
-            scanf("%lf", &temp);
-            carRentals += temp;
-        }
-    }while (option != '2');
+    printf("How much was the car rental:");
+    scanf("%lf", &carRentals);
 
     return carRentals;
 }
 
 double get_Private_Vehicle_Used(){
-    char option;
     double gasCost;
 
-    do{
-        printf("Select an option:");
-        printf("1 - Input a Gas Total");
-        printf("2 - Exit");
-        option = getChar();
-        getChar();
-
-        if(option == '1'){
-            double temp = 0;
-            printf("How much was the gas:");
-            scanf("%lf", &temp);
-            gasCost += temp;
-        }
-    }while (option != '2');
+    printf("How much was the gas:");
+    scanf("%lf", &gasCost);
 
     return gasCost;
 }
 
 double get_Round_Trip_Airfare(){
-    char option;
     double airplaneCost;
 
-    do{
-        printf("Select an option:");
-        printf("1 - Input an Airplane Ticket Cost");
-        printf("2 - Exit");
-        option = getChar();
-        getChar();
-
-        if(option == '1'){
-            double temp = 0;
-            printf("How much was the ticket:");
-            scanf("%lf", &temp);
-            airplaneCost += temp;
-        }
-    }while (option != '2');
+    printf("How much was the ticket:");
+    scanf("%lf", &airplaneCost);
 
     return airplaneCost;
 }
 
 double get_Parking_Fees(){
-    char option;
     double parkingFees;
 
-    do{
-        printf("Select an option:");
-        printf("1 - Input an Parking Fee");
-        printf("2 - Exit");
-        option = getChar();
-        getChar();
-
-        if(option == '1'){
-            double temp = 0;
-            printf("How much was parking:");
-            scanf("%lf", &temp);
-            parkingFees += temp;
-        }
-    }while (option != '2');
+    printf("How much was parking:");
+    scanf("%lf", &parkingFees);
 
     return parkingFees;
 }
 
 double get_Taxi_Fees(){
-    char option;
     double taxiFees;
 
-    do{
-        printf("Select an option:");
-        printf("1 - Input an Taxi Fee");
-        printf("2 - Exit");
-        option = getChar();
-        getChar();
-
-        if(option == '1'){
-            double temp = 0;
-            printf("How much was parking:");
-            scanf("%lf", &temp);
-            taxiFees += temp;
-        }
-    }while (option != '2');
+    printf("How much was parking:");
+    scanf("%lf", &taxiFees);
 
     return taxiFees;
 }
