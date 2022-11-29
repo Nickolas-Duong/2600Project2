da780e9b375958b3ee516b3aee894a9df1c75db5
Dylan Oliva <djoliva@cpp.edu>
2022-11-28 18:16:20 -0800

    added trip.h file and implemented a method to call the other methods in trip.c

--- src/trip.c
+++ src/trip.c
@@ -1,23 +1,61 @@
-#include <stdio.h>  
-#include <stdlib.h>
+#include "../include/trip.h"
+
+double trip(double total_expenses){
+    char option = 'z';
+
+    while(option != '0'){
+        printf("Input the number of the option you would like to input:");
+        printf("1 - Car Rental Cost");
+        printf("2 - Gas Cost");
+        printf("3 - Taxi Cost");
+        printf("4 - Parking Fees");
+        printf("5 - Airfare Cost");
+        printf("Any key to exit");
+        option = getChar();
+        getChar();
+
+        switch (option)
+        {
+        case '1':
+            total_expenses += get_Car_Rentals();
+            break;
+        case '2':
+            total_expenses += get_Private_Vehicle_Used();
+            break;
+        case '3':
+            total_expenses += get_Taxi_Fees();
+            break;
+        case '4':
+            total_expenses += get_Parking_Fees();
+            break;
+        case '5':
+            total_expenses += get_Round_Trip_Airfare();
+        }
+    }
+
+    return total_expenses;
+}
 
 double get_Car_Rentals(){
     char option;
+    double carRentals;
 
-    printf("Did you rent a car?\n1 - Yes\n2 - No");
-    option = getChar();
-    getChar();
+    do{
+        printf("Select an option:");
+        printf("1 - Input a Car Rental Fee");
+        printf("2 - Exit");
+        option = getChar();
+        getChar();
 
-    if(option == '1'){
-        int days = getNumber_of_day_on_Trip();
-        double price;
-        printf("How much did the car cost for each day?");
-        scanf("%lf", &price);
-        int total = price * days;
-        return total;
-    }else{
-        return 0;
-    }
+        if(option == '1'){
+            double temp = 0;
+            printf("How much was the car rental:");
+            scanf("%lf", &temp);
+            carRentals += temp;
+        }
+    }while (option != '0');
+
+    return carRentals;
 }
 
 double get_Private_Vehicle_Used(){
@@ -59,6 +97,7 @@ void get_Parking_Fees(int total_days, double* total_expenses, double* total_allo
     double price = 0;
     printf("How much was parking per day?");
     scanf("%lf", &price);
+    price *= total_days;
     total_expenses += price;
 }
 
